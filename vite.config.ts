3095744ef4770b1c52ef35ccfb143bfd007c6208
cindylin1211 <106232561+cindylin1211@users.noreply.github.com>
2025-09-20 22:36:04 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,10 +16,3 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/aws-gov-funding-site/', // 
-  plugins: [react()],
-})
